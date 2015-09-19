f3f553190832607094f7d973c3e7fecdefd2b628
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-09-19 23:48:48 +0200

    Refractored main class now it exits main when the initial loading is complete, allowing java service wrappers to detect when the application is started.

--- src/gameconsole/HttpSnoopServer.java
+++ src/gameconsole/HttpSnoopServer.java
@@ -8,33 +8,39 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
-/**
- * An HTTP server that sends back the content of the received HTTP request
- * in a pretty plaintext form.
- */
 public final class HttpSnoopServer {
+
+    private static EventLoopGroup bossGroup;
+    private static EventLoopGroup workerGroup;
+    private static ServerBootstrap b;
+    private static GameConsole g;
+    private static Channel ch;
+    
+    private final static int PORT = 8080;
 
     public static void main(String[] args) throws Exception {
         // Configure the server.
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-        try {
-            ServerBootstrap b = new ServerBootstrap();
-            GameConsole g = new GameConsole();
-            b.group(bossGroup, workerGroup)
-             .channel(NioServerSocketChannel.class)
-             .handler(new LoggingHandler(LogLevel.INFO))
-             .childHandler(new HttpSnoopServerInitializer(new GameConsole()));
+        bossGroup = new NioEventLoopGroup(1);
+        workerGroup = new NioEventLoopGroup();
+        b = new ServerBootstrap();
+        g = new GameConsole();
+        b.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new HttpSnoopServerInitializer(new GameConsole()));
 
-            Channel ch = b.bind(8080).sync().channel();
+        ch = b.bind(PORT).sync().channel();
 
-            System.err.println("Open your web browser and navigate to http://127.0.0.1:8080/");
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
 
-            ch.closeFuture().sync();
-            g.shutdown();
-        } finally {
-            bossGroup.shutdownGracefully();
-            workerGroup.shutdownGracefully();
-        }
+            @Override
+            public void run() {
+                ch.close().syncUninterruptibly();
+                g.shutdown();
+                bossGroup.shutdownGracefully();
+                workerGroup.shutdownGracefully();
+            }
+        }));
+
     }
 }
